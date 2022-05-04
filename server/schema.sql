2ba7fbf7221c7450ca1b1e23c50be61ba3fb507a
Gene_Yang <genehcyang@gmail.com>
2022-05-03 21:03:39 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,22 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id int not null,
+  roomname char not null,
+  primary key (id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int not null,
+  username char not null,
+  text_message text not null,
+  room_id int not null,
+  created_at datetime,
+  primary key (id),
+  foreign key (room_id) references rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
